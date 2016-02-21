24c2cd41813b9d88fb82f38b1425ae8b25eda6f1
Honza Kasik <honza.kasik1@seznam.cz>
2016-02-21 13:53:17 +0100

    Initial commit

---
+++ Polygon.py
@@ -0,0 +1,19 @@
+import json
+
+
+class Polygon:
+    points = []
+
+    def __init__(self):
+        pass
+
+    def add_point(self, point):
+        self.points.append(point)
+
+    def get_wrapper_dict(self):
+        return {'polygon': self.points}
+
+    def to_JSON(self):
+        return json.dumps(self, default=lambda o: o.__dict__,
+                          sort_keys=True, indent=4)
+        # return json.dumps(self.get_wrapper_dict())
---
+++ PolygonGroup.py
@@ -0,0 +1,24 @@
+import json
+
+
+class PolygonGroup:
+    bounding_box = None
+    polygons = []
+
+    def __init__(self, bounding_box):
+        self.bounding_box = bounding_box
+
+    def add_polygon(self, polygon):
+        self.polygons.append(polygon)
+
+    def get_polygons(self):
+        return self.polygons
+
+    def to_JSON(self):
+        return json.dumps(self, default=lambda o: o.__dict__,
+                          sort_keys=True, indent=4)
+        # wrapper_list = []
+        # for polygon in self.polygons:
+        #    wrapper_list.append(polygon.get_wrapper_dict())
+        # wrapper_dict = {self.bounding_box: wrapper_list}
+        # return json.dumps(wrapper_dict)
