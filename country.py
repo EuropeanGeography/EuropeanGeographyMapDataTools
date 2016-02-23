859b917b87e27c3475de91a1bd37a564bbf24dca
Honza Kasik <honza.kasik1@seznam.cz>
2016-02-23 20:26:41 +0100

    Moved methods from parser to Country class, added docstring

---
+++ country.py
@@ -0,0 +1,81 @@
+import json
+import copy
+
+import elements
+
+try:
+    from country_bounding_boxes import (
+        country_subunits_containing_point,
+        country_subunits_by_iso_code
+    )
+except ImportError:  # ignore
+    print("Error importing country_bounding_boxes, is this package installed?")
+
+
+class Country:
+    name = None
+    iso2 = None
+    tags = {}
+    polygons = []
+
+    def __init__(self, polygons, tags):
+        self.polygons = polygons
+        self.tags = tags
+        self.name = self.tags['name']
+        self.iso2 = self.tags['ISO3166-1:alpha2']
+
+    def assign_polygons(self, polygons):
+        self.polygons = polygons
+
+    def to_json(self):
+        wrapper = copy.deepcopy(self.tags)
+        wrapper['bounding-boxes'] = [c.bbox for c in country_subunits_by_iso_code(self.iso2)]
+        wrapper['polygons'] = self.polygons
+        return json.dumps(wrapper)
+
+    @staticmethod
+    def extract_from_relation(source, nodes, ways):
+        """
+        Extracts instance of Country from instance of Relation
+
+        :param source: source instance of Relation, from which the Country instance will be extracted
+        :param nodes: list of Node instances, containing all parsed nodes from document from which the source comes
+        :param ways: list of Way instances, containing all parsed ways from document from which the source comes
+        :return: instance of country containing all necessary information
+        """
+        referenced_nodes = []
+        tags = {}
+        polygons = []
+        for member in source.members:
+            # type of referenced element is node
+            if member.type == elements.ReferencingType.node:
+                referenced_nodes.append(nodes[member.ref].to_dict())
+            # type of referenced element is way
+            elif member.type == elements.ReferencingType.way:
+                referenced_way = ways[str(member.ref)]
+                nodes_of_referenced_way = []
+                for nd in referenced_way.nds:  # nd is element referencing to nodes
+                    # extracting nodes referenced by way referenced by relation
+                    nodes_of_referenced_way.append(nodes[nd.ref].to_dict())
+                polygons.append(nodes_of_referenced_way)
+        for tag in source.tags:
+            tags[tag.key] = tag.value
+        return Country(polygons=polygons, tags=tags)
+
+    @staticmethod
+    def extract_from_way(source, nodes):
+        """
+        Extracts instance of Country from instance of Way
+
+        :param source: source instance of Way, from which the Country instance will be extracted
+        :param nodes: list of Node instances, containing all parsed nodes from document from which the source comes
+        :return: instance of country containing all necessary information
+        """
+        referenced_nodes = []
+        tags = {}
+        # nd is element referencing to nodes
+        for nd in source.nds:
+            referenced_nodes.append(nodes[nd.ref].to_dict())
+        for tag in source.tags:
+            tags[tag.key] = tag.value
+        return Country(polygons=[referenced_nodes], tags=tags)
--- parser.py
+++ parser.py
@@ -3,7 +3,7 @@ import os
 import elements
 import xml.etree.ElementTree as ElementTree
 
-from Country import Country
+from country import Country
 
 
 class Parser:
@@ -24,7 +24,6 @@ class Parser:
     def __load_to_memory(self):
         for element in self.__root.iter():
             if element.tag == 'relation':
-                print("Found element '{0}'!".format(element))
                 relation = elements.Relation(element)
                 self.relations[relation.relation_id] = relation
             elif element.tag == 'node':
@@ -33,44 +32,14 @@ class Parser:
             elif element.tag == 'way':
                 way = elements.Way(element)
                 self.ways[way.way_id] = way
-                print('Found way {0}'.format(way.to_str()))
 
     def __extract_country(self, source, source_type):
         if source.is_representing_country():
-            # print('extracting {0} with id={1} and tags={2}'.format(str(source), source.way_id, source.tags[1].value))
             if source_type == 'way':
-                nodes = []
-                tags = {}
-                for nd in source.nds:
-                    nodes.append(self.nodes[nd.ref].to_dict())
-                for tag in source.tags:
-                    tags[tag.key] = tag.value
-                print('obtained tags ' + str(tags))
-                return Country(polygons=[nodes], tags=tags)
+                return Country.extract_from_way(source=source, nodes=self.nodes)
             elif source_type == 'relation':
-                return self.__extract_from_relation(source)
+                return Country.extract_from_relation(source=source, nodes=self.nodes, ways=self.ways)
         raise ValueError('Source {0} is not representing country!'.format(source))
-
-    def __extract_from_relation(self, source):
-        print('extracting {0} relation'.format(source))
-        nodes = []
-        ways = []
-        tags = {}
-        polygons = []
-        for member in source.members:
-            if member.type == elements.ReferencingType.node:
-                nodes.append(self.nodes[member.ref].to_dict())
-            elif member.type == elements.ReferencingType.way:
-                referenced_way = self.ways[str(member.ref)]
-                nodes_of_referenced_way = []
-                ways.append(referenced_way)
-                for nd in referenced_way.nds:
-                    nodes_of_referenced_way.append(self.nodes[nd.ref].to_dict())
-                polygons.append(nodes_of_referenced_way)
-        for tag in source.tags:
-            tags[tag.key] = tag.value
-        print('extracted {0} relation'.format(source))
-        return Country(polygons=polygons, tags=tags)
 
     def __write_country_to_file(self, source, source_type):
         path = 'output/'
@@ -80,15 +49,11 @@ class Parser:
         output = open(path + country.iso2 + '.json', 'w')
         output.write(country.to_json())
         output.close()
-        print('wrote {0} country with tags {1}'.format(country.name, country.tags))
 
     def __process_tags(self):
         for relation in self.relations.values():
-            print('processing {0} relation'.format(relation))
             if relation.is_representing_country():
-                print('Representing country {0} relation'.format(relation))
                 self.__write_country_to_file(source=relation, source_type='relation')
         for way in self.ways.values():
             if way.is_representing_country():
-                print('processing {0} with id={1} and tags={2}'.format(str(way), way.way_id, way.tags[1].value))
                 self.__write_country_to_file(source=way, source_type='way')
